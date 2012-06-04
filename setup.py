016c4930c7bc2a04d17398a13ec2375b0d5b91ea
Jiehong <ma.jiehong@gmail.com>
2012-06-04 19:43:18 +0200

    Update for setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
-
 from distutils.core import setup
 
 setup(name="Zhudi",
       version="1.1",
       description="Zhudi is a graphical interface to xDICT dictionaries",
-      long_description="Zhudi is a graphical interface to CEDICT, CFDICT,"+
-      "HanDeDict and ChE-Dicc. Therefore, it allows you to have a Chinese to"+
-      "English, French, German and Spanish dictionary.",
+      long_description="Have a look at README.",
       author="Jiehong Ma",
       author_email="ma.jiehong at gmail",
       url="https://github.com/Jiehong/Zhudi",
       license="GPLv3",
-      py_modules=["main", "data", "gui", "pinyin_to_zhuyin_table"])
+      py_modules=["zhudi", "data", "gui", "pinyin_to_zhuyin_table"])
