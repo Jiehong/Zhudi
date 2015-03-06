3c0c0159d22b2f8d657201010f100a1f1961968e
murchik <mixturchik@gmail.com>
2015-03-06 21:10:38 +0800

    setuputils instead of distutils.

--- setup.py
+++ setup.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python
-from distutils.core import setup
 # coding: utf-8
+
+from setuptools import setup, find_packages
+
 
 setup(
     name="zhudi",
-    version="1.3",
+    version="1.4",
     description="Zhudi is a graphical interface to xDICT dictionaries",
     long_description="Have a look at README.",
     author="Jiehong Ma",
     author_email="ma.jiehong at gmail",
     url="https://github.com/Jiehong/Zhudi",
     license="GPLv3",
-    py_modules=["gui", "zhudi_data", "zhudi_processing", "zhudi_chinese_table"],
-    scripts=['zhudi'],
+    packages=find_packages(),
+    py_modules=[
+        "gui",
+        "cli",
+        "zhudi",
+        "zhudi_data",
+        "zhudi_processing",
+        "zhudi_chinese_table",
+    ],
+    scripts=[
+        'zhudi',
+        'zhu',
+    ],
     data_files=[('share/zhudi-data', ['zhudi-data/array30',
                                       'zhudi-data/cangjie5',
                                       'zhudi-data/wubi86'])])
